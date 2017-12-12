dc7eba5e627c2d0bb8704ca418c4d094ce928d71
alexSelez <seleznyov.alexander93@gmail.com>
2017-12-12 22:24:52 +0300

    Final 6.0

--- js/script.js
+++ js/script.js
@@ -4,26 +4,23 @@
     var din = popup.querySelector("[name=date-in]");
     var dout = popup.querySelector("[name=date-out]");
     var storage = localStorage.getItem("In");
-   
-      btn.addEventListener("click", function(evt){
-
-         if (popup.classList.contains("form-hide")) {
-          evt.preventDefault();
-          popup.classList.remove("form-hide");
-          popup.classList.remove("info-error");
-          popup.classList.add("form-show");
+    btn.addEventListener("click", function(evt) {
+        if (popup.classList.contains("form-hide")) {
+            evt.preventDefault();
+            popup.classList.remove("form-hide");
+            popup.classList.remove("info-error");
+            popup.classList.add("form-show");
+        } else {
+            evt.preventDefault();
+            popup.classList.remove("form-show");
+            popup.classList.add("form-hide");
         }
-        else{
-          evt.preventDefault();
-          popup.classList.remove("form-show");
-          popup.classList.add("form-hide");
-        }  
     });
-    form.addEventListener("submit", function (evt) {
-      if ((!din.value) || (!dout.value)){
-        evt.preventDefault();
-        popup.classList.remove("info-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("info-error");
-      }      
+    form.addEventListener("submit", function(evt) {
+        if ((!din.value) || (!dout.value)) {
+            evt.preventDefault();
+            popup.classList.remove("info-error");
+            popup.offsetWidth = popup.offsetWidth;
+            popup.classList.add("info-error");
+        }
     });
