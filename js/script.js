6f9d321029cc1dd4034edff6c255ccc0f4f192cb
alexSelez <seleznyov.alexander93@gmail.com>
2017-12-13 21:56:34 +0300

    Final 7.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-    var btn = document.querySelector(".form-search-btn-title");
-    var popup = document.querySelector(".form-search-content");
-    var form = document.querySelector("form");
-    var din = popup.querySelector("[name=date-in]");
-    var dout = popup.querySelector("[name=date-out]");
-    var storage = localStorage.getItem("In");
-    btn.addEventListener("click", function(evt) {
-        if (popup.classList.contains("form-hide")) {
-            evt.preventDefault();
-            popup.classList.remove("form-hide");
-            popup.classList.remove("info-error");
-            popup.classList.add("form-show");
-        } else {
-            evt.preventDefault();
-            popup.classList.remove("form-show");
-            popup.classList.add("form-hide");
-        }
-    });
-    form.addEventListener("submit", function(evt) {
-        if ((!din.value) || (!dout.value)) {
-            evt.preventDefault();
-            popup.classList.remove("info-error");
-            popup.offsetWidth = popup.offsetWidth;
-            popup.classList.add("info-error");
-        }
-    });
+var btn = document.querySelector(".form-search-btn-title");
+var popup = document.querySelector(".form-search-content");
+var form = document.querySelector("form");
+var din = popup.querySelector("[name=date-in]");
+var dout = popup.querySelector("[name=date-out]");
+var storage = localStorage.getItem("In");
+btn.addEventListener("click", function(evt) {
+  if (popup.classList.contains("form-hide")) {
+    evt.preventDefault();
+    popup.classList.remove("form-hide");
+    popup.classList.remove("info-error");
+    popup.classList.add("form-show");
+  } else {
+    evt.preventDefault();
+    popup.classList.remove("form-show");
+    popup.classList.add("form-hide");
+  }
+});
+form.addEventListener("submit", function(evt) {
+  if ((!din.value) || (!dout.value)) {
+    evt.preventDefault();
+    popup.classList.remove("info-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("info-error");
+  }
+});
